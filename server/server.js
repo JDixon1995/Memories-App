26e8666f853cb11fabb4bccd9cf037e46e467cf8
John D <jddhalogamer@gmail.com>
2022-07-26 20:01:49 -0500

    added root route.

--- server/server.js
+++ server/server.js
@@ -16,6 +16,10 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
+app.get('/', (req, res) => {
+	res.send('Hello Memories API.')
+})
+
 const CONNECTION_URL = `mongodb+srv://AdamJensen95:${process.env.DB_PW}@projectapp.z9zgl.mongodb.net/?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000
 
