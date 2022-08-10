248fd401b654c1d45e3360c62694c4ea64ffb55d
John D <jddhalogamer@gmail.com>
2022-08-09 20:39:33 -0500

    Auth form rough draft.

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,7 +1,41 @@
+import { Avatar, Button, Paper, Grid, Typography, Container } from  '@material-ui/core'
+import Input from './Input'
+import useStyles from './styles'
+
 const Auth = () => {
-	const state = null
+	const classes = useStyles()
+
+	const isSignUp = false
+
+	const handleSubmit = () => {
+		console.log('clicked')
+	}
+
+	const handleChange = () => {
+		console.log('changed')
+	}
+
   return (
-	<div>AUTH</div>
+	<Container component='main' maxWidth='xs'>
+		<Paper className={classes.paper} elevation={3}>
+			<Avatar className={classes.avatar}>
+				
+			</Avatar>
+			<Typography variant='h5'>{isSignUp ? 'Sign Up' : 'Login In'}</Typography>
+			<form className={classes.form} onSubmit={handleSubmit}>
+				<Grid container spacing={2}>
+					{
+						isSignUp && (
+							<>
+								<Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+              					<Input name="lastName" label="Last Name" handleChange={handleChange} half />
+							</>
+						)
+					}
+				</Grid>
+			</form>
+		</Paper>
+	</Container>
   )
 }
 export default Auth
